b9d35adc778c3a2bebae942c820fa95705b0ed5b
Paul's Repository <cnubigdata@gmail.com>
2025-09-27 01:32:05 +0900

    Update 01_vm_web_apache.cmd

--- 01_vm_web_apache.cmd
+++ 01_vm_web_apache.cmd
@@ -4,3 +4,11 @@ $ sudo su
 # sudo systemctl status apache2
 # cd /var/www/html
 # echo "VM Web Server: $(hostname -f) - IP: $(hostname -I | awk '{print $1}')" > index.html
+
+
+# SSH 서비스 상태 확인
+sudo systemctl status ssh
+sudo systemctl status sshd
+
+# SSH 서비스 재시작
+sudo systemctl restart ssh
