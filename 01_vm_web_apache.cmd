cd593e56399595dfe285fba5aeaed37641cd906c
Paul's Repository <cnubigdata@gmail.com>
2025-10-04 12:20:23 +0900

    Update 01_vm_web_apache.cmd

--- 01_vm_web_apache.cmd
+++ 01_vm_web_apache.cmd
@@ -1,9 +1,12 @@
-$ sudo su
-# sudo apt update -y
-# sudo apt install apache2 -y
-# sudo systemctl status apache2
-# cd /var/www/html
-# echo "VM Web Server: $(hostname -f) - IP: $(hostname -I | awk '{print $1}')" > index.html
+sudo su
+sudo apt update -y
+
+sudo apt install apache2 -y
+
+sudo systemctl status apache2
+
+cd /var/www/html
+echo "VM Web Server: $(hostname -f) - IP: $(hostname -I | awk '{print $1}')" > index.html
 
 
 # apache web seraver
